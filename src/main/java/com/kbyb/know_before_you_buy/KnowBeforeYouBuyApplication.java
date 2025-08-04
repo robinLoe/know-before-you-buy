eb66745e1ec6a94d216e76fa34df1cc4a40ca2b0
robinLoe <robin-loesekrug@gmx.de>
2025-08-04 11:22:09 +0200

    Setup of SprinBoot Project and connection to postgre db

---
+++ src/main/java/com/kbyb/know_before_you_buy/KnowBeforeYouBuyApplication.java
@@ -0,0 +1,13 @@
+package com.kbyb.know_before_you_buy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowBeforeYouBuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowBeforeYouBuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kbyb/know_before_you_buy/KnowBeforeYouBuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbyb.know_before_you_buy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowBeforeYouBuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
